b7644f0099d5bec8a81b6d883e7f0ca252f6105d
Antonín Vondrovic <71491435+antonin-vondrovic-edu@users.noreply.github.com>
2020-12-14 10:10:39 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> jed = new ArrayList<>();
+        List<Integer> dva = new ArrayList<>();
+
+        Random rand = new Random();
+        for (int i = 0; i < 100; i++) {
+            jed.add(rand.nextInt(100));
+        }
+        for (int i = 0; i < 50; i++) {
+            dva.add(rand.nextInt(100));
+        }
+
+        Collections.sort(jed);
+        Collections.sort(dva);
+
+        for (int i = 0; i < 100; i++) {
+            if (jed.get(i) == jed.get(i++)) {
+                jed.set(i,rand.nextInt(100));
+            }
+        }
+
+        Collections.sort(jed);
+        Collections.sort(dva);
+
+        System.out.println("První pole - délka:"+jed.size());
+        for (Integer integer : jed) {
+            System.out.print(integer+",");
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("Druhé pole - délka:"+dva.size());
+        for (Integer integer : dva) {
+            System.out.print(integer+",");
+        }
+        System.out.println();
+        System.out.println();
+        if (jed.equals(dva)) {
+            System.out.println("Pole jsou stejná");
+        } else {
+            System.out.println("Pole nejsou stejná");
+        }
+
+    }
+}
