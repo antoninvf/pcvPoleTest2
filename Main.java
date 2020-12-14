52b13cdb3105295b947d51112e1afff6356fad8f
Antonín Vondrovic <71491435+antonin-vondrovic-edu@users.noreply.github.com>
2020-12-14 11:42:31 +0100

    :(

--- Main.java
+++ Main.java
@@ -23,24 +23,46 @@ public class Main {
         Collections.sort(jed);
         Collections.sort(dva);
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 50; i++) {
+            jed.remove(i);
+        }
+
+
+        for (int i = 0; i < jed.size(); i++) {
             if (jed.get(i) == jed.get(i++)) {
-                jed.set(i,rand.nextInt(100));
+                Collections.sort(jed);
+                jed.remove(i);
+                i--;
             }
         }
+        for (int i = 0; i < dva.size(); i++) {
+            if (dva.get(i) == dva.get(i++)) {
+                Collections.sort(dva);
+                dva.remove(i);
+                i--;
+            }
+        }
+
+        for (int i = 0; i < jed.size(); i++) {
+            if (jed.get(i) != dva.get(i)) {
+                int bweeejabreciim = jed.get(i);
+                dva.set(i, bweeejabreciim);
+            }
+        }
+
 
         Collections.sort(jed);
         Collections.sort(dva);
 
-        System.out.println("První pole - délka:"+jed.size());
+        System.out.println("První pole - délka:" + jed.size());
         for (Integer integer : jed) {
-            System.out.print(integer+",");
+            System.out.print(integer + ",");
         }
         System.out.println();
         System.out.println();
-        System.out.println("Druhé pole - délka:"+dva.size());
+        System.out.println("Druhé pole - délka:" + dva.size());
         for (Integer integer : dva) {
-            System.out.print(integer+",");
+            System.out.print(integer + ",");
         }
         System.out.println();
         System.out.println();
